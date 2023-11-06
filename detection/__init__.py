9e4ce829a2050ef2172bf2b193da43e9c5983e5e
Floric Slimani <floric.slimani@live.fr>
2023-11-06 16:14:21 +0100

    dev get_centroids_list

--- detection/__init__.py
+++ detection/__init__.py
@@ -7,4 +7,4 @@ This pbwrap subpackage wrapers around bigfish.detection subpackage.
 """
 
 from .detection_wrappers import spot_decomposition_nobckgrndrmv, detect_spots, iter_detect_spots
-from .clusters import cluster_detection
+from .clusters import cluster_detection, get_centroids_list
