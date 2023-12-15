3d0c2fd0cbafafb09cd6037d828948d8c4344487
Flo <floric.slimani@live.fr>
2023-12-15 11:11:01 +0100

    try correction bug : list to ndarray'

--- quantification/hub.py
+++ quantification/hub.py
@@ -108,10 +108,8 @@ def _centrosome_cell_quant(cell, voxel_size, dapi_stack, acquisition_id, centros
         try :
             cell_res = _main_cell_quant(cell=cell, voxel_size=voxel_size, dapi_stack=dapi_stack, acquisition_id=acquisition_id, compute_centrosome= True, centrosome_coords=centrosome_coords)
             cell_res.at[0, "centrosome_number"] = centrosome_number
-            print([tuple(coords) for coords in centrosome_coords])
             centrosome_coords = tuple([tuple(coords) for coords in centrosome_coords])
             cell_res["centrosome_coords"] = (np.array([tuple(coords) for coords in centrosome_coords], dtype= int),)
-            print(cell_res)
         except IndexError as error :
             print("centrosome coords : ", centrosome_coords)
             raise error
