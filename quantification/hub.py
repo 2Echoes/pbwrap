2625dcbe9e1a43ae23ffdd20ef31445ad5ca6d63
Flo <floric.slimani@live.fr>
2023-12-15 13:12:49 +0100

    saving coordinates as tuple in cell df

--- quantification/hub.py
+++ quantification/hub.py
@@ -112,6 +112,6 @@ def _centrosome_cell_quant(cell, voxel_size, dapi_stack, acquisition_id, centros
         cell_res = _main_cell_quant(cell=cell, voxel_size=voxel_size, dapi_stack=dapi_stack, acquisition_id=acquisition_id, compute_centrosome= True, centrosome_coords=centrosome_coords_2d)
         cell_res.at[0, "centrosome_number"] = centrosome_number
         centrosome_coords = tuple([tuple(coords) for coords in centrosome_coords])
-        cell_res["centrosome_coords"] = tuple([tuple(coords) for coords in centrosome_coords])
+        cell_res["centrosome_coords"] = (tuple([tuple(coords) for coords in centrosome_coords]),)
 
         return cell_res
