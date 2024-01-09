747c346836e2873c0cf8e8d704e2053b5ab1ca48
Flo <floric.slimani@live.fr>
2024-01-09 16:19:47 +0100

    debug

--- quantification/hub.py
+++ quantification/hub.py
@@ -117,7 +117,7 @@ def _centrosome_cell_quant(cell, voxel_size, dapi_stack, acquisition_id, centros
         cell_res["centrosome_coords"] = (tuple([tuple(coords) for coords in centrosome_coords]),)
         clusters_quant = _clusters_quant(cell)
 
-        cell_res = pd.concat([cell_res, clusters_quant], axis= 1)
+        # cell_res = pd.concat([cell_res, clusters_quant], axis= 1)
 
         return cell_res
 
