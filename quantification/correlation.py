89a89c704abdbf74667a4512d89688a907928562
Floric Slimani <floric.slimani@live.fr>
2024-01-16 11:16:29 +0100

    work on corr score

--- quantification/correlation.py
+++ quantification/correlation.py
@@ -17,7 +17,6 @@ def pearson_correlation(image1: np.ndarray, image2: np.ndarray) :
 
     return pearson_matrix[0,1]
 
-def detection_correlation(signal, spots) : 
-    if len(spots) == 0 : return 0
-    reconstructed_signal = np.zeros_like(signal)
+def correlation_score(pearson_correlation, random_correlation) : 
+    return (pearson_correlation - random_correlation) / (pearson_correlation + random_correlation)
     
