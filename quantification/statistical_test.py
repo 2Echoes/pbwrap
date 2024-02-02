1c1ddaa53c3d3c8ffae28266c78c44c4e83864eb
Flo <floric.slimani@live.fr>
2024-02-02 11:41:03 +0100

    tlw

---
+++ quantification/statistical_test.py
@@ -0,0 +1,22 @@
+import scipy.stats as stats
+
+
+def ANOVA(sample_list : list, return_p_value= True, return_f_stats= False) :
+    """
+    Performs ANOVA test (Analysis of Variance) using `scipy.stats.f_oneway` function.
+    
+    Returns
+    -------
+    p_value
+
+    f_stats
+    """
+
+    f_stats, p_value = stats.f_oneway(*sample_list)
+
+    if return_p_value and return_f_stats :
+        return p_value, f_stats
+    elif return_p_value :
+        return p_value
+    elif return_f_stats :
+        return f_stats
