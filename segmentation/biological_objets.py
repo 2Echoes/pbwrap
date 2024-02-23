f43a50e538828b5a5082536f541e9da6f0cee1e4
Floric Slimani <floric.slimani@live.fr>
2024-02-23 09:24:58 +0100

    new detection for centrosome in IF

--- segmentation/biological_objets.py
+++ segmentation/biological_objets.py
@@ -2,9 +2,10 @@ import numpy as np
 import bigfish.stack as stack
 import bigfish.segmentation as seg
 import scipy.ndimage as ndi
+from ..preprocessing import dog_filter, variance_filter
 from .utils import get_histogramm_highest_varation_value, unstack_slices, from2Dlabel_to3Dlabel, auto_LoG_threshold
 from .custom_functions import thresholding
-from ..utils import compute_anisotropy_coef, compute_voxel_volume, gaussian_kernel_size
+from ..utils import compute_voxel_volume, gaussian_kernel_size
 from ..errors import PbodySegmentationError
 from bigfish.stack import check_array, check_parameter
 from skimage.morphology import remove_small_objects
@@ -61,7 +62,7 @@ def pbody_segmentation(egfp, sigma = 2, threshold= None, thresh_penalty= 1, smal
     return egfp_label
 
 
-def centrosome_segmentation_candidate_regions(image, centrosome_size, voxel_size, gaussian_fit = 'FWTM', threshold_penalty = 1) :
+def centrosome_segmentation_candidate_regions(image, centrosome_size, voxel_size, gaussian_fit = 'FWTM', threshold_penalty = 1, DoG_filter_method=True, LoG_filter_method=False, variance_method=True,) :
     """
     1st built for centrosome pipeline, centrosome segmentation.
     This function segments candidate regions for centrosme segmentation. More precise segmentation is then performed when label is processed cell by cell.
@@ -76,14 +77,29 @@ def centrosome_segmentation_candidate_regions(image, centrosome_size, voxel_size
     """
 
 
-    centrosome_gaussian_size = gaussian_kernel_size(centrosome_size, voxel_size, width= gaussian_fit)
     centrosome_voxel_ratio = [c_size / v_size for c_size, v_size in zip(centrosome_size, voxel_size)]
     centrosome_pixel_volume = compute_voxel_volume(centrosome_voxel_ratio)
+    
+    if DoG_filter_method and not LoG_filter_method :
+        im_dog = dog_filter(image, kernel_size_1=1, kernel_size_2=3, voxel_size=voxel_size)
+        threshold = np.percentile(im_dog, 99.96)
+        mask = thresholding(im_dog, threshold)
+        
+    elif LoG_filter_method and not DoG_filter_method :
+        im_log = stack.log_filter(image, sigma= centrosome_gaussian_size)
+        centrosome_gaussian_size = gaussian_kernel_size(centrosome_size, voxel_size, width= gaussian_fit)
+        flat_log = im_log.flatten()
+        threshold = auto_LoG_threshold(flat_log) * threshold_penalty
+        mask = thresholding(im_log, threshold)
 
-    im_log = stack.log_filter(image, sigma= centrosome_gaussian_size)
-    flat_log = im_log.flatten()
-    threshold = auto_LoG_threshold(flat_log) * threshold_penalty
-    mask = thresholding(im_log, threshold)
+    else : raise ValueError("Choose either DoG_filter_method (recommended for Alexa 647 fluorophore) or LoG_filter_method (recommended for EGFP)")
+    
+    if variance_method :
+        im_var = variance_filter(image, (1,1,1))
+        threshold = np.percentile(im_dog, 99.96)
+        mask_ = thresholding(im_dog, threshold)
+
+    
     mask = remove_small_objects(mask, round(centrosome_pixel_volume / 2**3)) # removing objects smaller than the half of expected centrosome
     labels,_ = ndi.label(mask)
 
